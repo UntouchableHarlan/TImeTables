57a93f67d9db6bf5ff46848ddfa77d5d21c7cdd4
Salomon <salomonpluviose@yahoo.com>
2016-09-30 15:23:02 -0400

    App that lets you view your time tables from 1 - 12 using a UI slider

--- TimeTables/ViewController.swift
+++ TimeTables/ViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 13
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "timeCell")
+        
+        cell.textLabel?.text = String(indexPath.row * Int(slider.value))
+        
+        return cell
+        
+    }
+    
+    @IBAction func sliderChanged(_ sender: AnyObject) {
+        table.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
